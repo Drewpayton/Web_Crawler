2f093caa84633e958aa12a63377f38b9795ddf29
Drew Payton <drewpayton18@Drews-MacBook-Pro-2.local>
2024-09-07 20:57:32 -0500

    Added the logic to the normalizeURL function to normalize a url passing through

--- normalize_url.go
+++ normalize_url.go
@@ -1,5 +1,20 @@
 package main
 
-func normalizeURL(url string) (string, error){
-	return "", nil
+import (
+	"net/url"
+)
+
+func normalizeURL(fullURL string) (string, error){
+	parsedURL, err := url.Parse(fullURL)
+	if err != nil {
+		return "", err
+	}
+
+	normalizedString := parsedURL.Host + parsedURL.Path
+	if string(normalizedString[len(normalizedString) - 1]) == "/" {
+		normalizedString = normalizedString[:len(normalizedString) - 1]
+	}
+
+
+	return normalizedString, nil
 }
